20ebde4c4f6b89c2e537f10d3765ef930a015483
Vita-lika <vitalinat548@gmail.com>
2025-03-17 14:01:07 +0000

    vita

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
 import os
-from random import randint
 import asyncio
+from dotenv import load_dotenv, find_dotenv
 from loguru import logger
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from dotenv import find_dotenv, load_dotenv
+import requests
+from bs4 import BeautifulSoup
+from random import choice
 
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = -1002617178955
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 
 async def main():
     logger.add("file.log",
                format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-               rotation="3 days", backtrace=True, diagnose=True)
+               rotation="3 days",
+               backtrace=True,
+               diagnose=True)
 
-    bot = Bot(TOKEN)
+    bot = Bot(token=TOKEN)
     logger.info("Бот создан")
     dp = Dispatcher()
     logger.info("Диспетчер создан")
 
-    async def send_random_number():
+    async def send_random_joke():
         while True:
             try:
-                random_number = randint(1, 1000)
-                await bot.send_message(CHANNEL_ID,
-                                       f"Случайное число: {random_number}")
-                logger.info(f"Отправлено число: {random_number}")
+                response = requests.get('https://www.anekdot.ru/random/anekdot/')
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    jokes = soup.find_all('div', class_='text')
+
+                    random_joke = choice(jokes).text.strip()
+                    anekdot = random_joke
+                else:
+                    anekdot = "Не удалось получить анекдот"
+
+                await bot.send_message(CHANNEL_ID, f"Анекдот: {anekdot}")
+                logger.info(f"Опублекован анекдот: {anekdot}")
             except Exception as e:
-                logger.error(f"Ошибка при отправке сообщения: {e}")
+                logger.error(f"Ошибка при отправке сообщения {e}")
+
             await asyncio.sleep(30)
 
     @dp.message(Command("start"))
-    async def start_command(message: types.Message):
-        await message.answer(
-            "Бот запущен! Случайные числа будут отправляться в канал.")
+    async def send_welcome(message: types.Message):
+        await message.answer("Бот запущен! Он будет отправлять анекдоты!")
+        logger.info("Бот запущен")
 
-    task = asyncio.create_task(send_random_number())
+    task = asyncio.create_task(send_random_joke())
 
     try:
         await dp.start_polling(bot)
@@ -46,7 +59,6 @@ async def main():
         task.cancel()
         await bot.session.close()
         logger.info("Бот остановлен")
-
 
 if __name__ == '__main__':
     asyncio.run(main())
